d6091dc7f22e99acc7a593f695799e72e6f0c373
josephverbeck <joseph.verbeck@nih.gov>
2016-04-06 12:11:20 -0400

    Initial commit for backend heavy processor api

---
+++ app/jobs/treatment_job.rb
@@ -0,0 +1,26 @@
+class TreatmentJob
+
+    @queue = :treatment_arm
+
+    def self.perform(treatment_arm)
+      # begin
+      #   treatment_arm = treatment_arm.symbolize_keys
+      #   if TreatmentArm.where(:_id => treatment_arm[:id]).exists?
+      #     update(treatment_arm)
+      #   else
+      #     insert(treatment_arm)
+      #   end
+      # rescue => error
+      #   p error
+      # end
+    end
+
+    def self.update(treatment_arm)
+
+    end
+
+    # def self.insert(treatment_arm)
+    #   treatment_arm_model = TreatmentArm.new(treatment_arm)
+    #   treatment_arm_model.save
+    # end
+end
