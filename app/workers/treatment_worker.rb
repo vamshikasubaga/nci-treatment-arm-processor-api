41297773e694a95f3d96b7258a86876d95d73261
josephverbeck <joseph.verbeck@nih.gov>
2016-04-22 14:36:33 -0400

    Fixing issue with treatment_worker not throwing error when save fails...and slowing down basic_treatment_arm update rate

--- app/workers/treatment_worker.rb
+++ app/workers/treatment_worker.rb
@@ -12,7 +12,9 @@ class TreatmentWorker
         else
           insert(treatment_arm)
         end
+        ack!
       rescue => error
+        reject!
         p error
       end
     end
@@ -20,15 +22,18 @@ class TreatmentWorker
     def update(treatment_arm)
       if TreatmentArm.where(:treatment_arm_id => treatment_arm[:treatment_arm_id]).and(:version => treatment_arm[:version]).exists?
         p "Treatment_arm (#{treatment_arm[:treatment_arm_id]}) version (#{treatment_arm[:version]}) already exists"
-        reject!
       else
         insert(treatment_arm)
       end
     end
 
     def insert(treatment_arm)
-      treatment_arm_model = TreatmentArm.new(treatment_arm)
-      treatment_arm_model.save
-      ack!
+      begin
+        treatment_arm_model = TreatmentArm.new(treatment_arm)
+        treatment_arm_model.save
+      rescue => error
+        p "Failed to said treatment arm with error #{error}"
+        reject!
+      end
     end
 end
--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -1,6 +1,6 @@
 require 'rufus-scheduler'
 
 thread = Rufus::Scheduler.singleton
-thread.every '1s' do
+thread.every '3s' do
   BasicTreatmentArmJob.perform_later
 end
