ce130789a04fca7dd13a29a76b681a9bfaba7a18
futo82 <tony.fu@nih.gov>
2016-05-04 10:29:48 -0400

    integrate code climate test reporter and simplecov to the project

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,6 +18,15 @@ require 'rails_helper'
 # users commonly want.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+
+require "codeclimate-test-reporter"
+require 'simplecov'
+require 'simplecov-rcov'
+
+SimpleCov.formatter = SimpleCov::Formatter::RcovFormatter
+SimpleCov.start 'rails'
+CodeClimate::TestReporter.start
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
