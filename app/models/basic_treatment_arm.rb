114540816a6514839f6f877c50e8dc5fe546ee6e
josephverbeck <joseph.verbeck@nih.gov>
2016-05-09 16:07:31 -0400

    Removing commented out fields

--- app/models/basic_treatment_arm.rb
+++ app/models/basic_treatment_arm.rb
@@ -2,19 +2,6 @@
 class BasicTreatmentArm
   include Aws::Record
 
-  # table :name => :basic_treatment_arm_dev, :key => :treatment_arm_id, :read_capacity => 10, :write_capacity => 10
-  # field :treatment_arm_id
-  # field :treatment_arm_name
-  # field :current_patients
-  # field :former_patients
-  # field :not_enrolled_patients
-  # field :pending_patients
-  # field :treatment_arm_status
-  # field :date_created, :datetime
-  # field :date_opened, :datetime
-  # field :date_closed, :datetime
-  # field :date_suspended, :datetime
-
   set_table_name "ta_basic_treatment_arm_dev"
 
   string_attr :treatment_arm_id, hash_key: true
