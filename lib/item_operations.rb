79b1ae397ef9b0db08f5a2ce808168ecf408cc70
josephverbeck <joseph.verbeck@nih.gov>
2016-05-25 16:20:40 -0400

    Adding in Aws::Record method that points it to the correct endpoint since we have 2 aws clients

--- lib/item_operations.rb
+++ lib/item_operations.rb
@@ -2,6 +2,17 @@
 module Aws
   module Record
 
+    module RecordClassMethods
+      alias_method :original_configure_client, :configure_client
+      def configure_client(opts = {})
+        opts = {
+            endpoint: ENV['aws_dynamo_endpoint'],
+            region: ENV['aws_region']
+        }
+        original_configure_client(opts)
+      end
+    end
+
     module ItemOperations
       module ItemOperationsClassMethods
         alias_method :original_find, :find
