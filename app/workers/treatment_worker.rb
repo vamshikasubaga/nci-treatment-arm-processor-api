77717ab94ce0b13843540ba5bbc6a0810d732082
josephverbeck <joseph.verbeck@nih.gov>
2016-07-11 15:36:42 -0400

    Adding in status_log appender

--- app/workers/treatment_worker.rb
+++ app/workers/treatment_worker.rb
@@ -33,7 +33,7 @@ class TreatmentWorker
 
   def get_cog_treatment_arm_status
     begin
-      results = HTTParty.get('http://localhost:3000/treatmentArms')
+      results = HTTParty.get(ENV["cog_url"] + ENV["cog_treatment_arms"])
       cog_arms_status = JSON.parse(results.body).deep_transform_keys!(&:underscore).symbolize_keys!
       cog_arms_status[:treatment_arms].each do | treatment_arm |
         update_status(treatment_arm)
@@ -48,18 +48,26 @@ class TreatmentWorker
       cog_treatment_arm.deep_transform_keys!(&:underscore).symbolize_keys!
       match_treatment_arm = TreatmentArm.find_by(cog_treatment_arm[:treatment_arm_id], cog_treatment_arm[:stratum_id]).first
       if(!match_treatment_arm.blank?)
-        if(match_treatment_arm.treatment_arm_status != "CLOSED")
+        if(match_treatment_arm.treatment_arm_status != "CLOSED" && match_treatment_arm.treatment_arm_status != cog_treatment_arm[:status])
           match_treatment_arm.treatment_arm_status = cog_treatment_arm[:status]
-          match_treatment_arm.status_log = match_treatment_arm.status_log.merge!({cog_treatment_arm[:status_date] => cog_treatment_arm[:status]}).to_h
+          match_treatment_arm.status_log = rewrite_status_log(match_treatment_arm.status_log, {cog_treatment_arm[:status_date] => cog_treatment_arm[:status]})
           match_treatment_arm.save
           Shoryuken.logger.info("Treatment arm #{cog_treatment_arm[:treatment_arm_id]} (#{cog_treatment_arm[:stratum_id]}) has been updated")
+        else
+          Shoryuken.logger.info("Treatment arm #{cog_treatment_arm[:treatment_arm_id]} (#{cog_treatment_arm[:stratum_id]}) is currently CLOSED or Already in the correct state")
         end
-        Shoryuken.logger.info("Treatment arm #{cog_treatment_arm[:treatment_arm_id]} (#{cog_treatment_arm[:stratum_id]}) is currently CLOSED")
       end
       Shoryuken.logger.info("No Treatment Arms status to update")
     rescue => error
       Shoryuken.logger.error("Failed to update TA status from COG with error #{error}")
     end
+  end
+
+  def rewrite_status_log(status_log, append_hash={})
+    new_log = {}
+    new_log.merge!(status_log)
+    new_log.merge!(append_hash)
+    new_log
   end
 
   #Refacto!
