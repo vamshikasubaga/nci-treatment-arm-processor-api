b32bca9b6c8f2b115e838fab850781ce75be7183
josephverbeck <joseph.verbeck@nih.gov>
2016-08-03 17:43:13 -0400

    Adding in failure states....also adding in update method that changes previous patien status so counts don't get confused

--- app/jobs/patient_job.rb
+++ app/jobs/patient_job.rb
@@ -1,12 +1,12 @@
 
 class PatientJob
 
+  attr_accessor :patient_assignment_message
+
   def perform(patient_assignment)
     begin
       patient_assignment.symbolize_keys!
-      if(!TreatmentArmPatient.find(patient_id: patient_assignment[:patient_id], date_assigned: patient_assignment[:date_assigned]).blank?)
-        raise ArgumentError, "Patient #{patient_assignment[:patient_id]} with assignment date #{patient_assignment[:date_assigned]}is already in database...ignoring"
-      end
+      fail_safe(patient_assignment)
       case patient_assignment[:patient_assignment_status]
         when "ON_TREATMENT_ARM"
           Shoryuken.logger.info("Recieved patient at state ON_TREATMENT_ARM : #{patient_assignment[:patient_id]}")
@@ -32,18 +32,47 @@ class PatientJob
   end
 
   def store_patient(patient_assignment)
+    if(TreatmentArmPatient.find_by(:patient_id => patient_assignment[:patient_id]).blank?)
+      insert(patient_assignment)
+    else
+      update(patient_assignment)
+    end
+    BasicTreatmentArmJob.new.perform
+  end
+
+  def update(patient_assignment)
     begin
-      patient_assignment.compact!
+      patient_ta = TreatmentArmPatient.find_by(:patient_id => patient_assignment[:patient_id]).sort_by{| pa_ta | pa_ta.date_assigned}.reverse.first
+      patient_ta.patient_assignment_status = "FORMERLY_ON_ARM_PROGRESSED"
+      patient_ta.save
+      insert(patient_assignment)
+    rescue => error
+      Shoryuken.logger.error("Failed to update TreatmentArmPatient with error: #{error.message}")
+    end
+  end
+
+  def insert(patient_assignment)
+    begin
       patient_model = TreatmentArmPatient.new
       json = patient_model.convert_model(patient_assignment).to_json
       patient_model.from_json(json)
       patient_model.save
       Shoryuken.logger.info("Patient #{patient_model.patient_id} was successfully saved for assignment to #{patient_model.treatment_arm_name}")
-      BasicTreatmentArmJob.new.perform
     rescue => error
       Shoryuken.logger.error("Failed to insert TreatmentArmPatient with error: #{error.message}")
     end
   end
 
+  def fail_safe(patient_assignment)
+    if(!TreatmentArmPatient.find(patient_id: patient_assignment[:patient_id], date_assigned: patient_assignment[:date_assigned]).blank?)
+      raise ArgumentError, "Patient #{patient_assignment[:patient_id]} with assignment date #{patient_assignment[:date_assigned]}is already in database...ignoring"
+    end
+    patient_ta = TreatmentArmPatient.find_by(patient_id: patient_assignment[:patient_id]).sort_by{| patient_ta | patient_ta.date_assigned}.reverse.first
+    if(!patient_ta.blank?)
+      if(patient_ta.date_assigned > Date.parse(patient_assignment[:date_assigned]))
+        raise ArgumentError, "Patient #{patient_assignment[:patient_id]} assignment is out of order.  Current: #{patient_ta.to_h} Received: #{patient_assignment}"
+      end
+    end
+  end
 
 end
