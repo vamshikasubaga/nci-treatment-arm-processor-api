381e3f17e12cff74b60e215bd4c7a46eca863b09
asampatoor <adithya.sampatoor@nih.gov>
2016-10-26 13:09:42 -0400

    Added Model Specs

--- spec/models/treatment_arm/treatment_arm_spec.rb
+++ spec/models/treatment_arm/treatment_arm_spec.rb
@@ -17,31 +17,44 @@ describe TreatmentArm do
   let(:treatment_arm) do
     ba = TreatmentArm.new
     ba.active = true
-    ba.name = "TestData"
-    ba.stratum_id = "EAY131"
-    ba.version = "EAY13102"
-    ba.description = "testDescription"
-    ba.target_id = "gene"
-    ba.target_name = "otherGene"
-    ba.gene = "FGHS"
-    ba.treatment_arm_status = "OPEN"
-    ba.study_id = "EAY131"
+    ba.treatment_arm_id = 'APEC1621-A'
+    ba.name = 'TestData'
+    ba.stratum_id = 'EAY131'
+    ba.version = 'EAY13102'
+    ba.description = 'testDescription'
+    ba.target_id = '113'
+    ba.target_name = 'Crizotinib'
+    ba.gene = 'ALK'
+    ba.treatment_arm_status = 'OPEN'
+    ba.study_id = 'APEC1621'
     ba.num_patients_assigned = 4
-    ba.date_created = "2014-02-30"
+    ba.date_created = '2014-02-30'
+    ba.date_opened = '2016-03-25'
     ba.assay_rules = []
     ba.treatment_arm_drugs = []
     ba.diseases = []
     ba.exclusion_drugs = []
+    ba.snv_indels = []
+    ba.non_hotspot_rules = []
+    ba.copy_number_variants = []
     ba.status_log = {}
+    ba.gene_fusions = []
+    ba.current_patients = 1
+    ba.pending_patients = 2
+    ba.not_enrolled_patients = 1
+    ba.former_patients = 2
     ba
   end
 
-  it "should be the correct class type for the variables" do
-    stub_client.stub_responses(:describe_table, {
-        table: {table_status: "ACTIVE"}
-    })
+  it 'should be the correct class type for the variables' do
+    stub_client.stub_responses(:describe_table,
+                                {
+                                  table: { table_status: 'ACTIVE' }
+                                }
+                              )
     treatment_arm.configure_client(client: stub_client)
     expect(treatment_arm.name).to be_kind_of(String)
+    expect(treatment_arm.treatment_arm_id).to be_kind_of(String)
     expect(treatment_arm.version).to be_kind_of(String)
     expect(treatment_arm.description).to be_kind_of(String)
     expect(treatment_arm.target_id).to be_kind_of(String)
@@ -49,40 +62,63 @@ describe TreatmentArm do
     expect(treatment_arm.gene).to be_kind_of(String)
     expect(treatment_arm.treatment_arm_status).to be_kind_of(String)
     expect(treatment_arm.num_patients_assigned).to be_kind_of(Integer)
+    expect(treatment_arm.treatment_arm_id).to be_kind_of(String)
     expect(treatment_arm.study_id).to be_kind_of(String)
     expect(treatment_arm.stratum_id).to be_kind_of(String)
     expect(treatment_arm.assay_rules).to be_kind_of(Array)
     expect(treatment_arm.treatment_arm_drugs).to be_kind_of(Array)
     expect(treatment_arm.diseases).to be_kind_of(Array)
     expect(treatment_arm.exclusion_drugs).to be_kind_of(Array)
-    expect(treatment_arm.status_log).to be_kind_of(Hash)
+    expect(treatment_arm.current_patients).to be_kind_of(Integer)
+    expect(treatment_arm.pending_patients).to be_kind_of(Integer)
+    expect(treatment_arm.not_enrolled_patients).to be_kind_of(Integer)
+    expect(treatment_arm.former_patients).to be_kind_of(Integer)
+    expect(treatment_arm.gene_fusions).to be_kind_of(Array)
+    expect(treatment_arm.snv_indels).to be_kind_of(Array)
+    expect(treatment_arm.non_hotspot_rules).to be_kind_of(Array)
+    expect(treatment_arm.copy_number_variants).to be_kind_of(Array)
+    expect(treatment_arm.date_created).to be_kind_of(String)
+    expect(treatment_arm.date_opened).to be_kind_of(String)
   end
 
-  it "should return the correct values" do
-    stub_client.stub_responses(:describe_table, {
-        table: {table_status: "ACTIVE"}
-    })
+  it 'should return the correct class type for the Boolean attribute' do
+    expect(treatment_arm.active).to be_kind_of(TrueClass)
+    treatment_arm.active = false
+    expect(treatment_arm.active).to be_kind_of(FalseClass)
+  end
+
+  it 'should return the correct values' do
+    stub_client.stub_responses(:describe_table,
+                                {
+                                  table: { table_status: 'ACTIVE' }
+                                }
+                              )
     treatment_arm.configure_client(client: stub_client)
     json = {
-        :treatment_arm_id => "TestData",
-        :version => "EAY13102",
-        :study_id => "EAY131",
-        :stratum_id => "EAY131",
-        :description => "testDescription",
-        :target_id => "gene",
-        :target_name => "otherGene",
-        :gene => "FGHS",
-        :treatment_arm_status => "OPEN",
-        :num_patients_assigned => 4,
-        :date_created => "2014-02-30",
-        :assay_rules => [],
-        :treatment_arm_drugs => [],
-        :diseases => [],
-        :exclusion_drugs => [],
-        :status_log => {Time.now.to_i.to_s => "OPEN"},
-    }
+             treatment_arm_id: 'APEC1621-A',
+             version: 'EAY13102',
+             study_id: 'APEC1621',
+             stratum_id: 'EAY131',
+             description: 'testDescription',
+             target_id: '113',
+             target_name: 'Crizotinib',
+             gene: 'ALK',
+             treatment_arm_status: 'OPEN',
+             num_patients_assigned: 4,
+             date_created: '2014-02-30',
+             assay_rules: [],
+             treatment_arm_drugs: [],
+             diseases: [],
+             exclusion_drugs: [],
+             gene_fusions: [],
+             snv_indels: [],
+             copy_number_variants: [],
+             non_hotspot_rules: [],
+             status_log: { Time.now.to_i.to_s => 'OPEN' }
+           }
     hash = TreatmentArm.new.convert_models(json)
     from_json_ta = TreatmentArm.new.from_json(hash.to_json)
+    expect(treatment_arm.treatment_arm_id).to eq(from_json_ta.treatment_arm_id)
     expect(treatment_arm.version).to eq(from_json_ta.version)
     expect(treatment_arm.study_id).to eq(from_json_ta.study_id)
     expect(treatment_arm.stratum_id).to eq(from_json_ta.stratum_id)
@@ -97,6 +133,27 @@ describe TreatmentArm do
     expect(treatment_arm.diseases).to eq(from_json_ta.diseases)
     expect(treatment_arm.exclusion_drugs).to eq(from_json_ta.exclusion_drugs)
     expect(from_json_ta.status_log).to be_truthy
+    expect(treatment_arm.snv_indels).to eq(from_json_ta.snv_indels)
+    expect(treatment_arm.non_hotspot_rules).to eq(from_json_ta.non_hotspot_rules)
+    expect(treatment_arm.copy_number_variants).to eq(from_json_ta.copy_number_variants)
+    expect(treatment_arm.gene_fusions).to eq(from_json_ta.gene_fusions)
   end
 
+  it 'automatically declares treatment_arm_id' do
+    expect{treatment_arm.treatment_arm_id}.to_not raise_error
+  end
+
+  it "should be valid when an instance is created" do
+    expect(TreatmentArm.new).to be_truthy
+  end
+
+  it 'should be valid with valid attributes' do
+    treatment_arm = TreatmentArm.new(treatment_arm_id: 'APEC1621', stratum_id: 'EAY131', version: 'EAY13102')
+    expect(treatment_arm).to be_truthy
+  end
+
+  it 'should be of the correct instance' do
+    treatment_arm = TreatmentArm.new()
+    expect(treatment_arm).to be_an_instance_of(TreatmentArm)
+  end
 end
