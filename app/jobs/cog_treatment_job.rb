33207a068f41b3d37db08d443f743a1e7fd4f633
asampatoor <adithya.sampatoor@nih.gov>
2016-11-01 09:54:07 -0400

    Modified the Cog Status Update job a bit

--- app/jobs/cog_treatment_job.rb
+++ app/jobs/cog_treatment_job.rb
@@ -7,6 +7,7 @@ class CogTreatmentJob
       results = HTTParty.get(Rails.configuration.environment.fetch('cog_url') + Rails.configuration.environment.fetch('cog_treatment_arms'))
       cog_arms_status = JSON.parse(results.body).deep_transform_keys!(&:underscore).symbolize_keys!
       cog_arms_status[:treatment_arms].each do |treatment_arm|
+
         update_status(treatment_arm)
       end
       cog_arms_status[:treatment_arms]
@@ -18,15 +19,17 @@ class CogTreatmentJob
   def update_status(cog_treatment_arm)
     begin
       cog_treatment_arm.deep_transform_keys!(&:underscore).symbolize_keys!
-      match_treatment_arm = TreatmentArm.find_by(cog_treatment_arm[:treatment_arm_id], cog_treatment_arm[:stratum_id], nil, false).first
-      unless match_treatment_arm.blank?
-        if match_treatment_arm.treatment_arm_status != 'CLOSED' && match_treatment_arm.treatment_arm_status != cog_treatment_arm[:status]
-          match_treatment_arm.treatment_arm_status = cog_treatment_arm[:status]
-          match_treatment_arm.status_log = rewrite_status_log(match_treatment_arm.status_log, {cog_treatment_arm[:status_date] => cog_treatment_arm[:status]})
-          match_treatment_arm.save
-          Shoryuken.logger.info("The status for the TreatmentArm with id #{cog_treatment_arm[:treatment_arm_id]} & stratum_id #{cog_treatment_arm[:stratum_id]} has been successfully updated")
-        else
-          Shoryuken.logger.info("TreatmentArm with id #{cog_treatment_arm[:treatment_arm_id]} & stratum_id #{cog_treatment_arm[:stratum_id]} is currently CLOSED or already in the correct state")
+      match_treatment_arms = TreatmentArm.find_by(cog_treatment_arm[:treatment_arm_id], cog_treatment_arm[:stratum_id], nil, false)
+      match_treatment_arms.each do |match_treatment_arm|
+        unless match_treatment_arm.blank?
+          if match_treatment_arm.treatment_arm_status != 'CLOSED' && match_treatment_arm.treatment_arm_status != cog_treatment_arm[:status]
+            match_treatment_arm.treatment_arm_status = cog_treatment_arm[:status]
+            match_treatment_arm.status_log = rewrite_status_log(match_treatment_arm.status_log, {cog_treatment_arm[:status_date] => cog_treatment_arm[:status]})
+            match_treatment_arm.save
+            Shoryuken.logger.info("The status for the TreatmentArm with id #{cog_treatment_arm[:treatment_arm_id]} & stratum_id #{cog_treatment_arm[:stratum_id]} has been successfully updated")
+          else
+            Shoryuken.logger.info("TreatmentArm with id #{cog_treatment_arm[:treatment_arm_id]} & stratum_id #{cog_treatment_arm[:stratum_id]} is currently CLOSED or already in the correct state")
+          end
         end
       end
       Shoryuken.logger.info("No TreatmentArms status to update")
