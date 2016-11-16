21318219984a4d935cced4a6f61e224bf30d9a69
asampatoor <adithya.sampatoor@nih.gov>
2016-11-16 13:17:11 -0500

    Added TreatmentArm Model specs

--- spec/models/treatment_arm/treatment_arm_spec.rb
+++ spec/models/treatment_arm/treatment_arm_spec.rb
@@ -159,4 +159,143 @@ describe TreatmentArm do
     treatment_arm = TreatmentArm.new()
     expect(treatment_arm).to be_an_instance_of(TreatmentArm)
   end
+
+  describe 'Attributes' do
+
+    let(:record) do
+      Class.new do
+        include(Aws::Record)
+      end
+    end
+
+    describe '#initialize' do
+
+      let(:model) do
+        Class.new do
+          include(Aws::Record)
+          string_attr(:treatment_arm_id, hash_key: true)
+          string_attr(:stratum_id)
+          string_attr(:version)
+        end
+      end
+
+      it 'should allow attribute assignment at item creation time' do
+        item = model.new(treatment_arm_id: 'APEC1621-A')
+        expect(item.treatment_arm_id).to eq('APEC1621-A')
+        expect(item.stratum_id).to be_nil
+      end
+
+      it 'should allow assignment of multiple attributes at item creation' do
+        item = model.new(treatment_arm_id: 'APEC1621-A', stratum_id: '100', version: '2015-08-06')
+        expect(item.treatment_arm_id).to eq('APEC1621-A')
+        expect(item.stratum_id).to eq('100')
+        expect(item.version).to eq('2015-08-06')
+      end
+    end
+
+    describe 'Keys' do
+
+      it 'should be able to assign a hash key' do
+        record.string_attr(:treatment_arm_id, hash_key: true)
+        expect(record.hash_key).to eq(:treatment_arm_id)
+      end
+
+      it 'should be able to assign a hash and range key' do
+        record.string_attr(:treatment_arm_id, hash_key: true)
+        record.string_attr(:date_created, range_key: true)
+        expect(record.hash_key).to eq(:treatment_arm_id)
+        expect(record.range_key).to eq(:date_created)
+      end
+
+      it 'should reject assigning the same attribute as hash and range key' do
+        expect {
+          record.string_attr(:treatment_arm_id, hash_key: true, range_key: true)
+          }.to raise_error(ArgumentError)
+      end
+    end
+
+    describe 'Attributes' do
+
+      it 'should create dynamic methods around attributes' do
+        record.string_attr(:description)
+        x = record.new
+        x.description = 'This is the sample Description'
+        expect(x.description).to eq('This is the sample Description')
+      end
+
+      it 'should reject non-symbolized attribute names' do
+        expect { record.integer_attr("integer") }.to raise_error(ArgumentError)
+      end
+
+      it 'rejects collisions of db storage names with existing attr names' do
+        record.boolean_attr(:active, database_attribute_name: 'is_active_flag')
+        expect {
+          record.boolean_attr(:fail, database_attribute_name: 'active')
+          }.to raise_error(Aws::Record::Errors::NameCollision)
+      end
+
+      it 'rejects collisions of attr names with existing db storage names' do
+        record.boolean_attr(:active, database_attribute_name: 'is_active_flag')
+        expect {
+          record.boolean_attr(:is_active_flag, database_attribute_name: 'fail')
+          }.to raise_error(Aws::Record::Errors::NameCollision)
+      end
+
+      it 'should not allow duplicate assignment of the same attr name' do
+        record.string_attr(:target_name)
+        expect { record.datetime_attr(:target_name) }.to raise_error(
+          Aws::Record::Errors::NameCollision
+          )
+      end
+
+      it 'should typecast an integer attribute' do
+        record.integer_attr(:num_patients_assigned)
+        x = record.new
+        x.num_patients_assigned = '5'
+        expect(x.num_patients_assigned).to eq(5)
+      end
+
+      it 'should display a hash representation of attribute raw values' do
+        record.string_attr(:status_log)
+        x = record.new
+        x.status_log = '{"1479236708": "OPEN"}'
+        expect(x.to_h).to eq({ status_log: '{"1479236708": "OPEN"}' })
+      end
+
+      it 'should allow specification of a separate storage attribute name' do
+        record.string_attr(:active, database_attribute_name: 'is_active_flag')
+        record.string_attr(:name)
+        expect(record.attributes.storage_name_for(:active)).to eq('is_active_flag')
+        expect(record.attributes.storage_name_for(:name)).to eq('name')
+      end
+
+      it 'should reject storage name collisions' do
+        record.string_attr(:active, database_attribute_name: 'is_active_flag')
+        expect {
+          record.string_attr(:is_active_flag)
+          }.to raise_error(Aws::Record::Errors::NameCollision)
+          expect(record.attributes.present?(:is_active_flag)).to be_falsy
+      end
+
+      it 'should enforce uniqueness of storage names' do
+        record.string_attr(:active, database_attribute_name: 'is_active_flag')
+        expect {
+          record.string_attr(:treatment_arm_id, database_attribute_name: 'is_active_flag')
+          }.to raise_error(Aws::Record::Errors::NameCollision)
+      end
+
+      it 'should not allow collisions with reserved names' do
+        expect {
+          record.string_attr(:to_h)
+          }.to raise_error(Aws::Record::Errors::ReservedName)
+      end
+
+      it 'should allow reserved names to be used as custom storage names' do
+        record.string_attr(:active, database_attribute_name: 'is_active_flag')
+        item = record.new
+        item.active = true
+        expect(item.to_h).to eq({ active: true })
+      end
+    end
+  end
 end
