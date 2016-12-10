574741a7720fc56ed74648d598bdcf67dc2e4615
asampatoor <adithya.sampatoor@nih.gov>
2016-12-10 15:23:02 -0500

    Exposing more information in the /version call

--- app/controllers/versions_controller.rb
+++ app/controllers/versions_controller.rb
@@ -1,7 +1,23 @@
 class VersionsController < ApplicationController
   def version
     begin
-      render json: NciTreatmentArmProcessorApi::Application.VERSION
+      document = File.open('build_number.html', 'r')
+        hash = Hash.new
+        document.each_line do |line|
+          str = line.to_s
+          arr = str.split('=', 2)
+          hash.store(arr[0], arr[1])
+        end
+      @version = NciTreatmentArmProcessorApi::Application.VERSION
+      @rails_version = Rails::VERSION::STRING
+      @ruby_version = RUBY_VERSION
+      @running_on = hash['Commit'].present? ? hash['Commit'].tr("\n", "") : ""
+      @author = hash['Author'].present? ? hash['Author'].tr("\n", "") : ""
+      @travisbuild = hash['TravisBuild'].present? ? hash['TravisBuild'].tr("\n", "") : ""
+      @travisjob = hash['TravisJob'].present? ? hash['TravisJob'].tr("\n", "").to_i - 1 : ""
+      @dockerinstance = hash['Docker'].present? ? hash['Docker'].tr("\n", "") : ""
+      @buildtime = hash['BuildTime'].present? ? hash['BuildTime'].tr("\n", "") : ""
+      @environment = Rails.env
     rescue => error
       standard_error_message(error)
     end
--- spec/controllers/versions_controller_spec.rb
+++ spec/controllers/versions_controller_spec.rb
@@ -2,12 +2,6 @@ require 'rails_helper'
 
 describe VersionsController do
   describe 'GET #version' do
-    it 'Should return the API version' do
-     get :version
-     expect(response.body).to eq(NciTreatmentArmProcessorApi::Application.VERSION)
-     expect(response).to have_http_status(200)
-    end
-
     it 'should route to the correct controller' do
       expect(get: '/version').to route_to(controller: 'versions', action: 'version')
     end
