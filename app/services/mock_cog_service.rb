10d137f313b8be5e65b9b7f4ed3d80e679026163
asampatoor <adithya.sampatoor@nih.gov>
2017-01-05 16:21:37 -0500

    Adding logs to be more specific

--- app/services/mock_cog_service.rb
+++ app/services/mock_cog_service.rb
@@ -3,7 +3,7 @@ class MockCogService
   include HTTParty
 
   def self.perform
-    Shoryuken.logger.info("Mock COG service is Triggered to get the Latest TreatmentArm status")
+    Shoryuken.logger.info("#{self.class.name} | Mock COG service is Triggered to get the Latest TreatmentArm status")
     results = HTTParty.get(Rails.configuration.environment.fetch('mock_cog_url') + Rails.configuration.environment.fetch('cog_treatment_arms'))
     cog_arms_status = JSON.parse(results.body).deep_transform_keys!(&:underscore).symbolize_keys!
     cog_arms_status[:treatment_arms].each do |treatment_arm|
@@ -22,15 +22,15 @@ class MockCogService
             match_treatment_arm.treatment_arm_status = cog_treatment_arm[:status]
             match_treatment_arm.status_log = rewrite_status_log(match_treatment_arm.status_log, {cog_treatment_arm[:status_date] => cog_treatment_arm[:status]})
             match_treatment_arm.save
-            Shoryuken.logger.info("The status for the TreatmentArm with treatment_arm_id '#{cog_treatment_arm[:treatment_arm_id]}' & stratum_id '#{cog_treatment_arm[:stratum_id]}' has been successfully updated")
+            Shoryuken.logger.info("#{self.class.name} | The status for the TreatmentArm with treatment_arm_id '#{cog_treatment_arm[:treatment_arm_id]}' & stratum_id '#{cog_treatment_arm[:stratum_id]}' has been successfully updated")
           else
-            Shoryuken.logger.info("TreatmentArm with treatment_arm_id '#{cog_treatment_arm[:treatment_arm_id]}' & stratum_id '#{cog_treatment_arm[:stratum_id]}' is currently CLOSED or already in the correct state")
+            Shoryuken.logger.info("#{self.class.name} | TreatmentArm with treatment_arm_id '#{cog_treatment_arm[:treatment_arm_id]}' & stratum_id '#{cog_treatment_arm[:stratum_id]}' is currently CLOSED or already in the correct state")
           end
         end
       end
-      Shoryuken.logger.info("No TreatmentArms status to update")
+      Shoryuken.logger.info("#{self.class.name} | No TreatmentArms status to update")
     rescue => error
-      Shoryuken.logger.error("Failed to update TreatmentArm status from Mock COG with error #{error}::#{error.backtrace}")
+      Shoryuken.logger.error("#{self.class.name} | Failed to update TreatmentArm status from Mock COG with error #{error}::#{error.backtrace}")
     end
   end
 
