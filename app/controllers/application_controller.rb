7994c32d1c2f6c55d0b244332bf311f6652ac393
asampatoor <adithya.sampatoor@nih.gov>
2017-01-11 12:02:16 -0500

    Moving the error messages to the Application Controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   # protect_from_forgery with: :exception
 
+  def standard_error_message(error)
+    logger.error error.message
+    render json: error.to_json, status: 500
+  end
 end
--- app/controllers/versions_controller.rb
+++ app/controllers/versions_controller.rb
@@ -22,11 +22,4 @@ class VersionsController < ApplicationController
       standard_error_message(error)
     end
   end
-
-  private
-
-  def standard_error_message(error)
-    logger.error error.message
-    render json: error.to_json, status: 500
-  end
 end
