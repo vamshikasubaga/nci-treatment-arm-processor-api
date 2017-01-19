30ba7cea56212d452187e56fa0207624fba21a47
asampatoor <adithya.sampatoor@nih.gov>
2017-01-19 15:54:02 -0500

    Changing to use the block version of File.open

--- app/controllers/versions_controller.rb
+++ app/controllers/versions_controller.rb
@@ -1,23 +1,24 @@
+# Versions Controller, Gets Triggered when /version call is hit
 class VersionsController < ApplicationController
   def version
     begin
-      document = File.open('build_number.html', 'r')
-      hash = Hash.new
-      document.each_line do |line|
-        str = line.to_s
-        arr = str.split('=', 2)
-        hash.store(arr[0], arr[1])
+      File.open('build_number.html', 'r') do |document|
+        @hash = {}
+        document.each_line do |line|
+          arr = line.split('=', 2)
+          @hash.store(arr[0], arr[1])
+        end
+        document.close
       end
-      document.close
       @version = NciTreatmentArmProcessorApi::Application.version
       @rails_version = Rails::VERSION::STRING
       @ruby_version = RUBY_VERSION
-      @running_on = hash['Commit'].present? ? hash['Commit'] : ''
-      @author = hash['Author'].present? ? hash['Author'] : ''
-      @travisbuild = hash['TravisBuild'].present? ? hash['TravisBuild'] : ''
-      @travisjob = hash['TravisBuildID'].present? ? hash['TravisBuildID'] : ''
-      @dockerinstance = hash['Docker'].present? ? hash['Docker'] : ''
-      @buildtime = hash['BuildTime'].present? ? hash['BuildTime'] : ''
+      @running_on = @hash['Commit'].present? ? @hash['Commit'] : ''
+      @author = @hash['Author'].present? ? @hash['Author'] : ''
+      @travisbuild = @hash['TravisBuild'].present? ? @hash['TravisBuild'] : ''
+      @travisjob = @hash['TravisBuildID'].present? ? @hash['TravisBuildID'] : ''
+      @dockerinstance = @hash['Docker'].present? ? @hash['Docker'] : ''
+      @buildtime = @hash['BuildTime'].present? ? @hash['BuildTime'] : ''
       @environment = Rails.env
     rescue => error
       standard_error_message(error)
