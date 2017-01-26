0fcabae9162fb889b5e474539e13bb9512b69605
asampatoor <adithya.sampatoor@nih.gov>
2017-01-26 16:11:37 -0500

    Fix style issues & refactored the code

--- app/models/treatment_arm/treatment_arm.rb
+++ app/models/treatment_arm/treatment_arm.rb
@@ -41,91 +41,91 @@ class TreatmentArm
   integer_attr :version_not_enrolled_patients
   integer_attr :version_pending_patients
 
-  def self.find_by(id=nil, stratum_id=nil, version=nil, to_hash=true)
+  def self.find_by(id = nil, stratum_id = nil, version = nil, to_hash = true)
     query = {}
     query.merge!(build_scan_filter(id, stratum_id, version))
     if append_and?(!id.nil?, !stratum_id.nil?, !version.nil?)
-      query.merge!(conditional_operator: 'AND')
+      query[:conditional_operator] = 'AND'
     end
     if to_hash
-      self.scan(query).collect(&:to_h)
+      scan(query).collect(&:to_h)
     else
-      self.scan(query).entries
+      scan(query).entries
     end
   end
 
-  def self.build_scan_filter(id=nil, stratum_id=nil, version=nil)
+  def self.build_scan_filter(id = nil, stratum_id = nil, version = nil)
     query = { scan_filter: {} }
     unless id.nil?
-      query[:scan_filter].merge!('treatment_arm_id' => { comparison_operator: 'EQ', attribute_value_list: [id] })
+      query[:scan_filter]['treatment_arm_id'] = { comparison_operator: 'EQ', attribute_value_list: [id] }
     end
     unless stratum_id.nil?
-      query[:scan_filter].merge!('stratum_id' => { comparison_operator: 'EQ', attribute_value_list: [stratum_id] })
+      query[:scan_filter]['stratum_id'] = { comparison_operator: 'EQ', attribute_value_list: [stratum_id] }
     end
     unless version.nil?
-      query[:scan_filter].merge!('version' => { comparison_operator: 'EQ', attribute_value_list: [version] })
+      query[:scan_filter]['version'] = { comparison_operator: 'EQ', attribute_value_list: [version] }
     end
     query
   end
 
-  def self.append_and?(a=false, b=false, c=false)
+  def self.append_and?(a = false, b = false, c = false)
     (a && (b || c)) || (b && (c || a)) || (c && (a || b))
   end
 
   def self.build_cloned(treatment_arm)
-    return {
-             active: true,
-             treatment_arm_id: treatment_arm[:treatment_arm_id],
-             name: treatment_arm[:name],
-             version: treatment_arm[:version],
-             study_id: treatment_arm[:study_id],
-             stratum_id: treatment_arm[:stratum_id],
-             description: treatment_arm[:description],
-             target_id: treatment_arm[:target_id],
-             target_name: treatment_arm[:target_name],
-             gene: treatment_arm[:gene],
-             assay_rules: treatment_arm[:assay_rules],
-             treatment_arm_status: treatment_arm[:treatment_arm_status],
-             date_created: DateTime.current.getutc.to_s,
-             date_opened: treatment_arm[:date_opened].blank? ? Time.now : treatment_arm[:date_opened],
-             num_patients_assigned: treatment_arm[:num_patients_assigned],
-             treatment_arm_drugs: treatment_arm[:treatment_arm_drugs],
-             diseases: treatment_arm[:diseases],
-             exclusion_drugs: treatment_arm[:exclusion_drugs],
-             snv_indels: treatment_arm[:snv_indels],
-             non_hotspot_rules: treatment_arm[:non_hotspot_rules],
-             copy_number_variants: treatment_arm[:copy_number_variants],
-             gene_fusions: treatment_arm[:gene_fusions],
-             status_log: treatment_arm[:status_log].blank? ? { Time.now.to_i.to_s => 'OPEN' } : treatment_arm[:status_log]
-           }
+    {
+      active: true,
+      treatment_arm_id: treatment_arm[:treatment_arm_id],
+      name: treatment_arm[:name],
+      version: treatment_arm[:version],
+      study_id: treatment_arm[:study_id],
+      stratum_id: treatment_arm[:stratum_id],
+      description: treatment_arm[:description],
+      target_id: treatment_arm[:target_id],
+      target_name: treatment_arm[:target_name],
+      gene: treatment_arm[:gene],
+      assay_rules: treatment_arm[:assay_rules],
+      treatment_arm_status: treatment_arm[:treatment_arm_status],
+      date_created: DateTime.current.getutc.to_s,
+      date_opened: treatment_arm[:date_opened].blank? ? Time.now : treatment_arm[:date_opened],
+      num_patients_assigned: treatment_arm[:num_patients_assigned],
+      treatment_arm_drugs: treatment_arm[:treatment_arm_drugs],
+      diseases: treatment_arm[:diseases],
+      exclusion_drugs: treatment_arm[:exclusion_drugs],
+      snv_indels: treatment_arm[:snv_indels],
+      non_hotspot_rules: treatment_arm[:non_hotspot_rules],
+      copy_number_variants: treatment_arm[:copy_number_variants],
+      gene_fusions: treatment_arm[:gene_fusions],
+      status_log: treatment_arm[:status_log].blank? ? { Time.now.to_i.to_s => 'OPEN' } : treatment_arm[:status_log]
+    }
   end
 
   def convert_models(treatment_arm)
-    return {
-             active: true,
-             treatment_arm_id: treatment_arm[:treatment_arm_id],
-             name: treatment_arm[:name],
-             version: treatment_arm[:version],
-             study_id: treatment_arm[:study_id],
-             stratum_id: treatment_arm[:stratum_id],
-             description: treatment_arm[:description],
-             target_id: treatment_arm[:target_id],
-             target_name: treatment_arm[:target_name],
-             gene: treatment_arm[:gene],
-             assay_rules: treatment_arm[:assay_rules],
-             treatment_arm_status: attributes_present(treatment_arm),
-             date_created: DateTime.current.getutc.to_s,
-             date_opened: treatment_arm[:date_opened].blank? ? Time.now : treatment_arm[:date_opened],
-             num_patients_assigned: treatment_arm[:num_patients_assigned],
-             treatment_arm_drugs: treatment_arm[:treatment_arm_drugs],
-             diseases: treatment_arm[:diseases],
-             exclusion_drugs: treatment_arm[:exclusion_drugs],
-             snv_indels: treatment_arm[:snv_indels],
-             non_hotspot_rules: treatment_arm[:non_hotspot_rules],
-             copy_number_variants: treatment_arm[:copy_number_variants],
-             gene_fusions: treatment_arm[:gene_fusions],
-             status_log: treatment_arm[:status_log].blank? ? { Time.now.to_i.to_s => 'OPEN' } : treatment_arm[:status_log]
-           }
+    {
+      active: true,
+      treatment_arm_id: treatment_arm[:treatment_arm_id],
+      name: treatment_arm[:name],
+      version: treatment_arm[:version],
+      study_id: treatment_arm[:study_id],
+      stratum_id: treatment_arm[:stratum_id],
+      description: treatment_arm[:description],
+      target_id: treatment_arm[:target_id],
+      target_name: treatment_arm[:target_name],
+      gene: treatment_arm[:gene],
+      assay_rules: treatment_arm[:assay_rules],
+      treatment_arm_status: attributes_present(treatment_arm),
+      date_created: DateTime.current.getutc.to_s,
+      date_opened: treatment_arm[:date_opened].blank? ? Time.now : treatment_arm[:date_opened],
+      num_patients_assigned: treatment_arm[:num_patients_assigned],
+      treatment_arm_drugs: treatment_arm[:treatment_arm_drugs],
+      diseases: treatment_arm[:diseases],
+      exclusion_drugs: treatment_arm[:exclusion_drugs],
+      snv_indels: treatment_arm[:snv_indels],
+      non_hotspot_rules: treatment_arm[:non_hotspot_rules],
+      copy_number_variants: treatment_arm[:copy_number_variants],
+      gene_fusions: treatment_arm[:gene_fusions],
+      status_log: treatment_arm[:status_log].blank? ? { Time.now.to_i.to_s => 'OPEN' } : treatment_arm[:status_log]
+    }
   end
 
   private
