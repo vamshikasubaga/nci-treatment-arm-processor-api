c0423f16352f79b15be62f901d9f1230c5e1f0a2
asampatoor <adithya.sampatoor@nih.gov>
2017-02-08 19:22:13 -0500

    Make a check before uploading a diff version of the TA

--- app/jobs/treatment_job.rb
+++ app/jobs/treatment_job.rb
@@ -20,7 +20,11 @@ class TreatmentJob
   def insert_new_version(treatment_arm_hash, current_active_ta)
     new_treatment_arm = create_new_version(treatment_arm_hash)
     deactivate(current_active_ta) if new_treatment_arm.save
-    BasicTreatmentArmJob.new.perform(treatment_arm_hash[:treatment_arm_id], treatment_arm_hash[:stratum_id], nil)
+    BasicTreatmentArmJob.new.perform(treatment_arm_hash[:treatment_arm_id], treatment_arm_hash[:stratum_id], nil) if check_condition(current_active_ta)
+  end
+
+  def check_condition(treatment_arm)
+    true if treatment_arm.current_patients.present? || treatment_arm.former_patients.present? || treatment_arm.not_enrolled_patients.present? || treatment_arm.pending_patients.present?
   end
 
   def create_new_version(treatment_arm_hash)
