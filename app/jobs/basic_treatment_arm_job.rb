7816137b68339b58ff7305b6ea3965179676d5ba
asampatoor <adithya.sampatoor@nih.gov>
2017-02-22 11:40:26 -0500

    changed method names to be meaningful

--- app/jobs/basic_treatment_arm_job.rb
+++ app/jobs/basic_treatment_arm_job.rb
@@ -13,18 +13,18 @@ class BasicTreatmentArmJob
 
   def update(treatment_arm)
     treatment_arm.version_former_patients = find_patient_count_by_event(treatment_arm, 'FORMER_PATIENT')
-    treatment_arm.version_current_patients = find_patient_count_for_status(treatment_arm, ['ON_TREATMENT_ARM'])
+    treatment_arm.version_current_patients = find_patient_count_by_status(treatment_arm, ['ON_TREATMENT_ARM'])
     treatment_arm.version_not_enrolled_patients = find_patient_count_by_event(treatment_arm, 'NOT_ENROLLED')
-    treatment_arm.version_pending_patients = find_patient_count_for_status(treatment_arm, ['PENDING_APPROVAL'])
+    treatment_arm.version_pending_patients = find_patient_count_by_status(treatment_arm, ['PENDING_APPROVAL'])
     treatment_arm.former_patients = find_patient_count_by_event(treatment_arm, 'FORMER_PATIENT', true)
-    treatment_arm.current_patients = find_patient_count_for_status(treatment_arm, ['ON_TREATMENT_ARM'], true)
+    treatment_arm.current_patients = find_patient_count_by_status(treatment_arm, ['ON_TREATMENT_ARM'], true)
     treatment_arm.not_enrolled_patients = find_patient_count_by_event(treatment_arm, 'NOT_ENROLLED', true)
-    treatment_arm.pending_patients = find_patient_count_for_status(treatment_arm, ['PENDING_APPROVAL'], true)
+    treatment_arm.pending_patients = find_patient_count_by_status(treatment_arm, ['PENDING_APPROVAL'], true)
     treatment_arm.save
     Shoryuken.logger.info("#{self.class.name} | ===== Version & Stratum Statistics for TreatmentArm('#{treatment_arm.treatment_arm_id}'/'#{treatment_arm.stratum_id}'/'#{treatment_arm.version}') has been successfully updated =====")
   end
 
-  def find_patient_count_for_status(treatment_arm = nil, status_list = [], ignore_version = false)
+  def find_patient_count_by_status(treatment_arm = nil, status_list = [], ignore_version = false)
     query = {}
     query.merge!('treatment_arm_id' => { comparison_operator: 'CONTAINS', attribute_value_list: [treatment_arm.treatment_arm_id] })
     query.merge!('stratum_id' => { comparison_operator: 'EQ', attribute_value_list: [treatment_arm.stratum_id] })
